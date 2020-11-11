69b72dd86a611341155fa4064c23be9c914d8e95
Marina <marina.ssaito@gmail.com>
2020-11-11 06:51:03 -0300

    Splash screen + Login Activity

---
+++ app/src/main/java/com/example/desafio02/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.desafio02
+
+import android.content.Intent
+import android.graphics.Matrix
+import android.graphics.Matrix.ScaleToFit
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_restaurante.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+
+}
