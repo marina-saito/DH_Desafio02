2a9fbc878b8e133ee6ea26c795309ff749924a15
Marina <marina.ssaito@gmail.com>
2020-11-11 07:22:46 -0300

    Adjustments on Register activity

--- app/src/main/java/com/example/desafio02/LoginActivity.kt
+++ app/src/main/java/com/example/desafio02/LoginActivity.kt
@@ -2,12 +2,17 @@ package com.example.desafio02
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
 
     }
 }
